ff220d5eca10b92b867c511d12a41e5c55f453a2
dcbarros <dcbarros1@gmail.com>
2024-07-02 18:34:09 -0300

    feat: Adiciona rota do candidato

--- backend/src/candidate/entities/candidate.entity.ts
+++ backend/src/candidate/entities/candidate.entity.ts
@@ -1,1 +1,10 @@
-export class Candidate {}
+import { Candidate } from "@prisma/client";
+
+export class CandidateEntity implements Candidate{
+  id: bigint;
+  name: string;
+  email: string;
+  phone: string;
+  isSignIn: boolean;
+  createdAt: Date; 
+}
