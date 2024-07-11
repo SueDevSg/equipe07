0244660d41d1b66bcfc510fa143ed2be441a1316
dcbarros <dcbarros1@gmail.com>
2024-07-11 15:32:37 -0300

    feat: Modifica tabelas do prisma.schema

--- backend/prisma/migrations/20240711180241_init/migration.sql
+++ backend/prisma/migrations/20240711180241_init/migration.sql
@@ -1,9 +1,16 @@
+-- CreateEnum
+CREATE TYPE "Contribution" AS ENUM ('APPRENTICE', 'MENTOR', 'COLLABORATOR');
+
+-- CreateEnum
+CREATE TYPE "Level" AS ENUM ('STARTER', 'MIDLEVEL', 'ADVANCED');
+
 -- CreateTable
 CREATE TABLE "Candidate" (
     "id" BIGSERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "phone" VARCHAR(11) NOT NULL,
+    "contributionType" "Contribution" NOT NULL DEFAULT 'APPRENTICE',
     "isSignIn" BOOLEAN NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
@@ -14,18 +21,10 @@ CREATE TABLE "Candidate" (
 CREATE TABLE "Technologies" (
     "id" BIGSERIAL NOT NULL,
     "candidateId" BIGINT NOT NULL,
+    "level" "Level" NOT NULL DEFAULT 'STARTER',
     "name" TEXT NOT NULL,
 
     CONSTRAINT "Technologies_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "Level" (
-    "id" BIGSERIAL NOT NULL,
-    "technologyId" BIGINT NOT NULL,
-    "level" TEXT NOT NULL,
-
-    CONSTRAINT "Level_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -37,11 +36,5 @@ CREATE UNIQUE INDEX "Technologies_name_key" ON "Technologies"("name");
 -- CreateIndex
 CREATE INDEX "Technologies_candidateId_idx" ON "Technologies"("candidateId");
 
--- CreateIndex
-CREATE UNIQUE INDEX "Level_technologyId_key" ON "Level"("technologyId");
-
 -- AddForeignKey
 ALTER TABLE "Technologies" ADD CONSTRAINT "Technologies_candidateId_fkey" FOREIGN KEY ("candidateId") REFERENCES "Candidate"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Level" ADD CONSTRAINT "Level_technologyId_fkey" FOREIGN KEY ("technologyId") REFERENCES "Technologies"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
